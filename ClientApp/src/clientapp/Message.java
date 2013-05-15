b3150101105856da56007a417eb40d79c2bb7b54
jvegalo <jmiguelv24@gmail.com>
2013-05-15 17:32:30 -0500

    Initial commit

---
+++ ClientApp/src/clientapp/Message.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientapp;
+
+/**
+ *
+ * @author jose
+ */
+public class Message {
+    private int length;
+    private String text;
+    private User user;
+    
+}
---
+++ ClientApp/src/clientapp/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientapp;
+
+import connection.Connection;
+
+/**
+ *
+ * @author jose
+ */
+
+
+public class User {
+    private String user_name;
+    private String password;
+    private ChatRoom chat_room;
+    private Connection connection;
+    private Message message;
+}
